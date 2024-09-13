1712f2c28fc86a5a5aa8ee1a4368526d393d616f
ambarmishraa <ambarmishra740@gmail.com>
2024-09-13 23:42:48 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,102 +1,26 @@
-import React from "react";
-import News from "./Components/News.jsx";
-import Navbar from "./Components/Navbar.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Alert from './Components/Alert.jsx';
+import About from './Components/About.jsx';
+import Navbar from './Components/Navbar.jsx';
+import TextForms from './Components/TextForms.jsx';
+import useAdditional from "./Components/Additional.jsx";
 
-const App = () => {
+
+function App() {
+
+  const { mode, alert, toggleMode, showAlert } = useAdditional();
+
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <News
-                key="general"
-                pageSize={6}
-                country="in"
-                category="general"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/business"
-            element={
-              <News
-                key="business"
-                pageSize={6}
-                country="in"
-                category="business"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/entertainment"
-            element={
-              <News
-                key="entertainment"
-                pageSize={6}
-                country="in"
-                category="entertainment"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/science"
-            element={
-              <News
-                key="science"
-                pageSize={6}
-                country="in"
-                category="science"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/sports"
-            element={
-              <News
-                key="sports"
-                pageSize={6}
-                country="in"
-                category="sports"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/health"
-            element={
-              <News
-                key="health"
-                pageSize={6}
-                country="in"
-                category="health"
-              />
-            }
-          />
-          <Route
-            exact
-            path="/technology"
-            element={
-              <News
-                key="technology"
-                pageSize={6}
-                country="in"
-                category="technology"
-              />
-            }
-          />
-        </Routes>
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Navbar title="Case Converter" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <Routes>
+        <Route path="/" element={<TextForms textArea="Enter Your Text Below" mode={mode} showAlert={showAlert} />} />
+        <Route path="/about" element={<About mode={mode} toggleMode={toggleMode}/>} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,89 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import PropTypes from 'prop-types'
 
-const Navbar = () => {
+export default function Navbar(props) {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-dark bg-gradient">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            <strong style={{ color: "white" }}>Daily Digest</strong>
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="/"
-                  style={{ color: "white" }}>
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/business"
-                  style={{ color: "white" }}>
-                  Business
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/entertainment"
-                  style={{ color: "white" }}>
-                  Entertainment
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/health"
-                  style={{ color: "white" }}>
-                  Health
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/science"
-                  style={{ color: "white" }}>
-                  Science
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/sports"
-                  style={{ color: "white" }}>
-                  Sports
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link"
-                  to="/technology"
-                  style={{ color: "white" }}>
-                  Technology
-                </Link>
-              </li>
-            </ul>
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} `}
+    >
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          {props.title}
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                {/* {props.aboutText} */}
+                About Us
+              </Link>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-primary" type="submit" >
+              Search
+            </button>
+          </form>
+          <div className={`form-check form-switch mx-2 text-${props.mode === "light" ? "dark" : "light"} `} >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+              onClick={props.toggleMode}
+            />
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+              Enable Dark Mode
+            </label>
           </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
-};
-
-export default Navbar;
+}
